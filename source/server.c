0f095563641330a7d015549c3695ec2d48712712
afabbri <afabbri@student.42roma.it>
2023-05-23 12:48:11 +0200

    ciaone

---
+++ source/server.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afabbri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 11:52:44 by afabbri           #+#    #+#             */
+/*   Updated: 2023/05/23 12:45:09 by afabbri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../include/minitalk.h"
+
+void	ft_btoa(int sig)
+{
+	static int	bit;
+	static int	i;
+
+	if (sig == SIGUSR1)
+		i |= (0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_printf("%c", i);
+		bit = 0;
+		i = 0;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	(void)argv;
+	if (argc != 1)
+	{
+		ft_printf("Error\n");
+		return (1);
+	}
+	pid = getpid();
+	ft_printf("%d\n", pid);
+	while (argc == 1)
+	{
+		signal(SIGUSR1, ft_btoa);
+		signal(SIGUSR2, ft_btoa);
+		pause ();
+	}
+	return (0);
+}
